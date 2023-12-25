f33339d92a2b11b6b41671cbbd982e6c287949ba
MedHachami <moha.hach.9@gmail.com>
2023-12-25 16:50:44 +0100

    Update Chat.php

--- app/controllers/Chat.php
+++ app/controllers/Chat.php
@@ -7,56 +7,92 @@ class Chat implements MessageComponentInterface
 {
     protected $clients;
     protected $rooms;
+    
 
     public function __construct()
     {
         $this->clients = new \SplObjectStorage();
-        $this->rooms = [];
+        
     }
 
-    public function onOpen(ConnectionInterface $conn)
+    public function onOpen(ConnectionInterface $conn )
     {
+        
         $this->clients->attach($conn);
+        $conn->send(json_encode(["resourceId" => $conn->resourceId]));
         echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
-    $data = json_decode($msg, true);
-    // print_r($data);
-
-    if (isset($data['room'])) {
-        // This is a room message, handle it as before
+        $data = json_decode($msg, true);
+        
+        if($data == null){
+            return;
+        }
         $room = $data['room'];
+        
+        if (!empty($room)) {
+            switch ($data['type']) {
+                case 'join':
+                    $this->joinRoom($from, $room);
+                    break;
+                case 'msg':
+                    $this->broadcastToRoom($data , $from);
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+            
+        }
+    
 
+    
+    }
+
+    
+    function broadcastToRoom($data ,$sender){
+        $room = $data['room'];
+        $content = $data['content'];
+        $sentBy = $data['sentBy'];
+        $encodedMessage = json_encode(
+            ['content' => $content,
+             'room' => $room,
+              'sentBy'=>$sentBy,
+              'type'=>'msg',
+            ]);
+        
+        foreach ($this->rooms[$room] as $client) {
+            if ($client !== $sender) {
+                $client->send($encodedMessage);
+            }
+        }
+       
+    }
+
+    protected function joinRoom(ConnectionInterface $conn, $room)
+    {
         if (!isset($this->rooms[$room])) {
             $this->rooms[$room] = new \SplObjectStorage();
         }
 
-        $this->rooms[$room]->attach($from);
-        // print_r($this->rooms[$room]);
-        $numRecv = count($this->rooms[$room]) - 1;
+        $this->rooms[$room]->attach($conn);
+        
 
-        // Send the message to all clients in the room
-        $this->sendToRoom($room, $data['content'] , $data['user_name'], $from);
+        $numClients = count($this->rooms[$room]);
+        echo "Client {$conn->resourceId} joined room {$room}. Total clients in room: {$numClients}\n";
+    }
 
-        echo "Online in this room: " . ($numRecv + 1) . "\n";
-    } elseif (isset($data['recipient'])) {
-        // This is a private message
-        $recipientId = $data['recipient'];
-        $messageContent = $data['content'];
 
-        // Find the recipient in the connected clients
-        foreach ($this->clients as $client) {
-            if ($client->resourceId == $recipientId) {
-                // Send the private message to the recipient
-                $client->send(json_encode(['content' => $messageContent, 'room' => 'private']));
-                echo "Private message sent to user {$recipientId}\n";
-                break;
-            }
+    protected function leaveRoom(ConnectionInterface $conn, $room)
+    {
+        $this->rooms[$room]->detach($conn);
+        if ($this->rooms[$room]->count() === 0) {
+            unset($this->rooms[$room]);
         }
     }
-}
 
 
     public function onClose(ConnectionInterface $conn)
@@ -81,34 +117,7 @@ class Chat implements MessageComponentInterface
         $conn->close();
     }
 
-    protected function sendToRoom($room, $message,$user_name , $exclude)
-    {
-        $encodedMessage = json_encode(['content' => $message, 'room' => $room , 'user_name'=>$user_name]);
+    
 
-        foreach ($this->rooms[$room] as $client) {
-            if ($client !== $exclude) {
-                $client->send($encodedMessage);
-            }
-        }
-    }
-
-    protected function joinRoom(ConnectionInterface $conn, $room)
-    {
-        $joinMessage = json_encode([
-            'content' => "Joined room: $room",
-            'room' => $room
-        ]);
-
-        $this->sendToRoom($room, $joinMessage, $conn);
-    }
-
-    protected function leaveRoom(ConnectionInterface $conn, $room)
-    {
-        $leaveMessage = json_encode([
-            'content' => "Left room: $room",
-            'room' => $room
-        ]);
-
-        $this->sendToRoom($room, $leaveMessage, $conn);
-    }
+    
 }
