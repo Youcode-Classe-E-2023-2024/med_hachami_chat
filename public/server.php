17de0fd41ab10f40d8add93381400bfbcc9bba8a
MedHachami <moha.hach.9@gmail.com>
2023-12-25 16:50:49 +0100

    Update server.php

--- public/server.php
+++ public/server.php
@@ -1,8 +1,10 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+error_reporting(E_ALL);
 require_once '../app/SocketServer.php';
 require_once '../vendor/autoload.php';
 require_once '../app/controllers/Chat.php';
  $server = new SocketServer();
  $server->run();
- echo "running server";
+//  echo "running server";  
