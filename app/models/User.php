9f47b09f507357d0bbc384f295c8e8222e151c82
MedHachami <moha.hach.9@gmail.com>
2023-12-26 02:20:16 +0100

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -25,9 +25,59 @@ Class User{
         return ($row) ? true : false;
     }
 
-    public function getAllUsers(){
-      $this->db->query('SELECT id , full_name , email , imgUrl FROM user');
+    public function getNormalUser($userId){
+      $this->db->query('
+      SELECT u.id, u.full_name , u.email , u.imgUrl
+    FROM user u
+    LEFT JOIN Friendships fs ON (fs.userId1 = :userId AND fs.userId2 = u.id)
+                            OR (fs.userId1 = u.id AND fs.userId2 = :userId)
+    LEFT JOIN FriendRequests fr ON (fr.senderId = :userId AND fr.receiverId = u.id)
+                              OR (fr.senderId = u.id AND fr.receiverId = :userId)
+    WHERE fs.userId1 IS NULL 
+      AND fs.userId2 IS NULL
+      AND fr.senderId IS NULL
+      AND u.id <> :userId;
+      ');
+      $this->db->bind(':userId', $userId);
       return $this->db->resultSet();
+    }
+
+    public function getAlreadySentRequest($userId){
+      $this->db->query('
+      SELECT u.id, u.full_name , u.email , u.imgUrl
+      FROM user u
+      JOIN friendrequests fr ON u.id = fr.receiverId
+      WHERE fr.senderId = :userId 
+        AND fr.status = :status
+        AND NOT EXISTS (
+          SELECT 1
+          FROM Friendships fs
+          WHERE (fs.userId1 = :userId  AND fs.userId2 = u.id)
+             OR (fs.userId1 = u.id AND fs.userId2 = :userId )
+        )
+        AND u.id <> :userId ;  
+      ');
+      $this->db->bind(':userId', $userId);
+      $this->db->bind(':status', 'pending');
+      return $this->db->resultSet();
+    }
+
+    public function getMyFriends($userId){
+      $this->db->query('
+      SELECT U.id, U.full_name , u.email , u.imgUrl
+      FROM user U
+      JOIN Friendships F ON (U.id = F.userId1 OR U.id = F.userId2)
+      WHERE :userId IN (F.userId1, F.userId2) and U.id <>:userId;  
+      ');
+      $this->db->bind(':userId', $userId);
+      return $this->db->resultSet();
+    }
+
+    public function addFriendShip($senderId,$reveiverId){
+      $this->db->query("INSERT INTO friendrequests(senderId ,receiverId) VALUES(:senderId,:reveiverId)");
+      $this->db->bind(':senderId', $senderId);
+      $this->db->bind(':reveiverId', $reveiverId);
+      return $this->db->execute();
     }
 
     public function login($email, $password){
